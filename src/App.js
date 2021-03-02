894ff5a9031e3c256086907b5011abd6d34eb5df
Ebun O <remioludemi@gmail.com>
2021-03-02 02:30:43 -0500

    called api (works)

--- src/App.js
+++ src/App.js
@@ -6,18 +6,45 @@ import Footer from './Footer.js';
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      eachPlace: [],
+      query: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.apiCall = this.apiCall.bind(this)
   }
-
+  handleOnChange(event) {
+    this.setState({query: event.target.value})
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(event)
+    this.apiCall(this.state.query)
+    }
+  async apiCall() {
+    const reqOptions = {
+        method: 'GET',
+        headers: {
+          "x-rapidapi-key": "f0d62b0e00mshe149082a3348d33p1a9cf2jsnffb3733db8b2",
+	        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+	        "useQueryString": true
+        }
+    }
+    let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" + this.state.query, reqOptions)
+    response = await response.json()
+    console.log(response.Places)
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
         <Header title="Airport Flight Search"/>
         <div className="search-div">
-          <form>
-            <input type="text" placeholder="Search For the Cheapest Flights" name="search"/>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Search For the Cheapest Flights" name="search" onChange={(e)=>this.handleOnChange(e)} value={this.state.query} required/>
             <button type="submit">Submit</button>
           </form>
+          <h1 style={{color: "black"}}>{this.state.query}</h1>
         </div>
         </header>
         <Footer footercontent="Thank You for Using My Flight Search Application"/>
