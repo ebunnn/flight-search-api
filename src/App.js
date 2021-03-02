fee1681dac2070e57e1640091c8707a671e9f5c5
Ebun O <remioludemi@gmail.com>
2021-03-02 00:17:38 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      eachPlace: [],
+      error: null,
+      searchWords: null,
+      submit: ""
+    }
+    this.apiCall = this.apiCall.bind(this)
+  }
+
+  apiCall() {
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "f0d62b0e00mshe149082a3348d33p1a9cf2jsnffb3733db8b2",
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+      }
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <div className="search-div">
+          <form>
+            <input type="text" placeholder="Search For the Cheapest Flights" name="search"/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        </header>
+      </div>
+    );
+  }  
+  }
 
 export default App;
