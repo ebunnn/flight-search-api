5063a760cf4beed396102ad71d5ee351c7b31528
Ebun O <remioludemi@gmail.com>
2021-03-15 01:40:17 -0400

    added parameters and the table

--- src/App.js
+++ src/App.js
@@ -10,16 +10,32 @@ class App extends Component {
     this.state = {
       eachPlace: [],
       query: "",
-      currency: "",
+      currency: "USD",
+      destination: "LAX-sky",
+      location: "SFO-sky",
+      outboundDate: "2021-03-21",
       eachCurrency: [],
       eachDate: [],
       eachQuote: []
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.apiCall = this.apiCall.bind(this)
+    this.handleCurrency = this.handleCurrency.bind(this) 
   }
   handleOnChange(event) {
     this.setState({query: event.target.value})
+  }
+  handleCurrency(event) {
+    this.setState({currency: event.target.value})
+  }
+  handleDestination(event) {
+    this.setState({destination: event.target.value})
+  }
+  handleLocation(event) {
+    this.setState({location: event.target.value})
+  }
+  handleOutboundDate(event) {
+    this.setState({outboundDate: event.target.value})
   }
   handleSubmit(event) {
     event.preventDefault()
@@ -29,12 +45,12 @@ class App extends Component {
     const reqOptions = {
         method: 'GET',
         headers: {
-          "x-rapidapi-key": "f0d62b0e00mshe149082a3348d33p1a9cf2jsnffb3733db8b2",
+          "x-rapidapi-key": `${process.env.REACT_APP_API_KEY}`,
 	        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
 	        "useQueryString": true
         }
     }
-    let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/SFO-sky/LAX-sky/2021-03-21/?query=" + this.state.query, reqOptions)
+    let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/" + this.state.currency + "/" + "en-US/" + this.state.location + "/" + this.state.destination + "/" + this.state.outboundDate + "/?query=" + this.state.query, reqOptions)
     response = await response.json()
     this.setState({eachPlace: response.Places})
     this.setState({eachCurrency: response.Currencies})
@@ -52,7 +68,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <Header title="Airport Flight Search"/>
-        <div className="search-div">
+        <form className="currency-search" onSubmit={this.handleSubmit}>
+          <label>Currency (ex. USD or GBD):  </label>
+            <input type="text" name="currency" onChange={(e)=>this.handleCurrency(e)} value={this.state.currency} required/>
+          </form>
+          <form className="currency-search" onSubmit={this.handleSubmit}>
+          <label>Destination (ex. LAX-sky):  </label>
+            <input type="text" name="destination" onChange={(e)=>this.handleDestination(e)} value={this.state.destination} required/>
+          </form>
+          <form className="currency-search" onSubmit={this.handleSubmit}>
+          <label> Current Location (ex. SFO-sky):  </label>
+            <input type="text" name="destination" onChange={(e)=>this.handleLocation(e)} value={this.state.location} required/>
+          </form>
+          <form className="currency-search" onSubmit={this.handleSubmit}>
+          <label>Outbound Date (ex. 2021-03-21):  </label>
+            <input type="text" name="destination" onChange={(e)=>this.handleOutboundDate(e)} value={this.state.outboundDate} required/>
+          </form>
+        <div className="search-div" onSubmit={this.handleSubmit}>
           <form onSubmit={this.handleSubmit}>
             <input type="text" placeholder="Search For the Cheapest Flights" name="search" onChange={(e)=>this.handleOnChange(e)} value={this.state.query} required/>
             <button type="submit">Submit</button>
@@ -62,7 +94,7 @@ class App extends Component {
           <BrowseDates places={this.state.eachPlace} />
         </div>
         </header>
-        <Footer footercontent="Flight Search Application"/>
+        {/* <Footer footercontent="Flight Search Application"/> */}
       </div>
     );
   }  
--- src/BrowseDates.js
+++ src/BrowseDates.js
@@ -4,24 +4,63 @@ import './css/BrowseDates.css';
 function browseDates(props) {
     return(
         <div>
-            <div className="airport-labels">
-                {props.places.map(place => {
-                    return(
-                        <div key={place.PlaceId}>
-                            <h5>Airport Name(PlaceName): {place.PlaceName}</h5>
-                            <h5>Airport ID (PlaceID): {place.PlaceId}</h5>
-                            <h5>Country(CountryName): {place.CountryName}</h5>
-                            <h5>Country ID: {place.CountyId}</h5>
-                            <h5>Region ID: {place.RegionId}</h5>
-                            <h5>City ID(City ID): {place.CityId}</h5>
-                        </div>
-                    )
-                })}
-                
+            <table className="airport-labels">
+                <thead>
+                    <tr>
+                        <th>Airport Name</th>
+                        <th>Code</th> 
+                        <th>Country</th>
+                        <th>City</th>
+                        <th>City ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.places.map(place => {
+                            return(        
+                                <tr key={place.PlaceId}> 
+                                    <td>{place.Name}</td>
+                                    <td>{place.SkyscannerCode}-sky</td>
+                                    <td>{place.CountryName}</td>
+                                    <td>{place.CityName}</td>
+                                    <td>{place.CityId}</td> 
+                                    {/* <h5>Country ID: {place.CountyId}</h5>
+                                    <h5>Region ID: {place.RegionId}</h5> */}    
+                                </tr>   
+                           )
+                        })}
+                </tbody>
+            </table>
+                {/* <table className="airport-labels">
+                            <thead>
+                                <tr>
+                                    <th>Airport Name</th>
+                                    <th>Code</th> 
+                                    <th>Country</th>
+                                    <th>City</th>
+                                    <th>City ID</th>
+                                </tr>
+                            </thead>
+                                                    
+                            <tbody>
+                                {props.places.map(place => {
+                                    return(        
+                                        <div key={place.PlaceId}>
+                                            <tr> 
+                                                <td>(PlaceName): {place.Name}</td>
+                                                <td>(SkyscannerCode): {place.SkyscannerCode}-sky</td>
+                                                <td>(CountryName): {place.CountryName}</td>
+                                                <td>(City Name): {place.CityName}</td>
+                                                <td>(City ID): {place.CityId}</td> 
+                                                <h5>Country ID: {place.CountyId}</h5>
+                                                <h5>Region ID: {place.RegionId}</h5> 
+                                            </tr>           
+                                        </div>
+                                    )
+                                })}
+                            </tbody>
+                    </table> */}
             </div>
-            
-        </div>
-    )
+        )
 }
 
 export default browseDates;
